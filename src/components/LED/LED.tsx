18019a07e80ba674f5d74322c1d3dfd4e78db906
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2022-03-13 21:51:58 +0100

    Remove unused import

--- src/components/LED/LED.tsx
+++ src/components/LED/LED.tsx
@@ -1,4 +1,3 @@
-import { convertToObject } from "typescript";
 import "../../index.css"
 
 export type LEDProps = { 
@@ -15,12 +14,6 @@ export default function LED(props: LEDProps) {
     const now = new Date().getTime();
     const last_seen = new Date(props.last_seen).getTime();
     const seen = (now - last_seen) < 1000*60*60*8;
-    
+
     return <div className="led" style={{backgroundColor: seen?led_color:"#000000"}} title={`LED ${props.id}: ${props.owner}`}></div>;
 }
-
-//1631721979000 1645650542798 13928563798
-
-
-// 2021-09-13 17:11:07
-// -> date
