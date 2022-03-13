a597510ca0982317d16ebf1b590db51fd4277585
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2022-03-13 21:26:14 +0100

    Panel: Blackout LEDs of chatters that are not seen for at least 8 hours

--- src/components/LED/LED.tsx
+++ src/components/LED/LED.tsx
@@ -1,3 +1,4 @@
+import { convertToObject } from "typescript";
 import "../../index.css"
 
 export type LEDProps = { 
@@ -11,11 +12,11 @@ export type LEDProps = {
 export default function LED(props: LEDProps) {
 
     let led_color = props.color ? "#"+props.color.toString(16).padStart(6,"0").slice(0,6): "#000000";
-    const seen = new Date(props.last_seen).getTime();
     const now = new Date().getTime();
-    return <div className="led" style={{backgroundColor: led_color}} title={`LED ${props.id}: ${props.owner}`}>
-        
-    </div>;
+    const last_seen = new Date(props.last_seen).getTime();
+    const seen = (now - last_seen) < 1000*60*60*8;
+    
+    return <div className="led" style={{backgroundColor: seen?led_color:"#000000"}} title={`LED ${props.id}: ${props.owner}`}></div>;
 }
 
 //1631721979000 1645650542798 13928563798
