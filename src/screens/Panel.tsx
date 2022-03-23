3a9da796e81a3e9aea0b4200d4adea8ebcf0476c
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2022-03-23 21:28:41 +0100

    Panel: Load Array data in Effect instead of directly

--- src/screens/Panel.tsx
+++ src/screens/Panel.tsx
@@ -1,5 +1,5 @@
+import { useEffect } from 'react';
 import { useAsync } from 'react-async';
-import { Overlay, OverlayTrigger, Table, Tooltip } from 'react-bootstrap';
 import LED from '../components/LED';
 
 const loadLEDs = async () =>
@@ -7,7 +7,11 @@ const loadLEDs = async () =>
 
 
 export default function Panel() {
-    const { data, isLoading } = useAsync({ promiseFn: loadLEDs })
+    const { data, isLoading, run } = useAsync({ deferFn: loadLEDs })
+
+    useEffect(()=>{
+        run(loadLEDs)
+    },[run])
 
     if (isLoading) return (<div>Loading...</div>)
     if (data) {
@@ -17,20 +21,20 @@ export default function Panel() {
             let row: Array<JSX.Element> = [];
             for (let y = 0; y < 32; y++) {
                 const offset = x * 32 + y;
-                if (data[offset]) { 
+                if (data[offset]) {
                     row.push(<td className='led-table-row'>
                         <LED key={data[offset].id} {...data[offset]} />
                     </td>);
                 } else {
                     row.push(<td className='led-table-row'>
-                        <LED key="letze" animation={false} color={0} id={1024} owner={""} last_seen={""}  />
+                        <LED key="letze" animation={false} color={0} id={1024} owner={""} last_seen={""} />
                     </td>);
                 }
             }
             rows.push(row);
         }
 
-        return <div style={{display:'flex', justifyContent: "center"}}><table className='led-table'>
+        return <div style={{ display: 'flex', justifyContent: "center" }}><table className='led-table'>
             {rows.map((row: Array<JSX.Element>, key: number) =>
                 <tr key={key}>
                     {row.map((led) => led)}
