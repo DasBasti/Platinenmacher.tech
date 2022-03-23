c70bb5f1824bb96c75e18ae04ae622e37676bf5e
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2022-03-23 22:40:45 +0100

    PCB: Add dynamic loading of PCB Images list

--- src/screens/PCB.tsx
+++ src/screens/PCB.tsx
@@ -1,27 +1,42 @@
+import { useEffect, useState } from "react";
 import { useAsync } from "react-async";
-import { Container } from "react-bootstrap";
+import { Button, Container, Pagination } from "react-bootstrap";
 import PCBListElement from '../components/PCBListElement';
 import { PCBListElementProps } from "../components/PCBListElement/PCBListElement";
 
-var page=1;
-var num=10;
-const loadPCBs = async () => 
-    await fetch("/api/list?page="+page+"&num="+num).then(res => (res.ok ? res : Promise.reject(res))).then(res => res.json())
+const loadPCBs = async (props:any) => 
+    await fetch("/api/list?page="+props.page+"&num="+props.num).then(res => (res.ok ? res : Promise.reject(res))).then(res => res.json())
 
 
 export default function PCB() {
-    const { data, isLoading } = useAsync({ promiseFn: loadPCBs})
-
-    
+    const [page, setPage] = useState(1);
+    const [num, setNum] = useState(10);
+    const { data, isLoading, reload } = useAsync({ promiseFn:loadPCBs, page, num})
+   
+    useEffect(()=>{
+        reload()
+    },[reload, page])
 
     console.log(isLoading, data)
     if (isLoading) return (<div>Loading...</div>)
-    if (data) return (
+    if (data) {
+        let pages = [];
+        for (let number = 1; number <= Math.ceil(data.total/num); number++) {
+        pages.push(
+            <Pagination.Item key={number} active={number === page} onClick={()=>{setPage(number)}}>
+            {number}
+            </Pagination.Item>,
+        );
+        }
+        return (
+        <Container>
         <Container>
             {data.entries.map((pcb:PCBListElementProps, key:number)=>
                 <PCBListElement key={key} {...pcb} />
             )}
         </Container>
-    )
+        <Pagination style={{flexWrap:'wrap'}} >{pages}</Pagination>
+        </Container>
+    )}
     return (<div></div>);
 }
