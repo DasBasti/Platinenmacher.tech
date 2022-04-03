933d3efff4620d4a3c733816010580172545e7c8
Bastian Neumann <neumann.bastian@gmail.com>
2022-04-03 20:24:31 +0200

    Add icons to Linklist

--- src/screens/Blog.tsx
+++ src/screens/Blog.tsx
@@ -1,7 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitch, faYoutube, faWordpress, faWordpressSimple, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faPodcast } from "@fortawesome/free-solid-svg-icons";
+
 import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import ProjectFeed from "../components/ProjectFeed";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
 
 
 export default function Blog() {
@@ -29,11 +35,11 @@ export default function Blog() {
                 <h2>Links</h2>
                 <hr />
                 <ListGroup>
-                    <ListGroup.Item><a href="https://www.twitch.tv/platinenmacher">Twitch</a></ListGroup.Item>
-                    <ListGroup.Item><a href="https://www.youtube.com/channel/UCPyRzwxnI9-pL5-50JkxY5Q">Youtube</a></ListGroup.Item>
-                    <ListGroup.Item><a href="https://kurzschluss-blog.de/">Blog</a></ListGroup.Item>
-                    <ListGroup.Item><a href="http://kurzschlussjunkies.de/">Podcast</a></ListGroup.Item>
-                    <ListGroup.Item><a href="https://github.com/DasBasti">GitHub</a></ListGroup.Item>
+                    <ListGroup.Item style={{border:0}}><a className="linkListLinks" href="https://www.twitch.tv/platinenmacher"><FontAwesomeIcon icon={faTwitch as IconProp} /> Twitch</a></ListGroup.Item>
+                    <ListGroup.Item style={{border:0}}><a className="linkListLinks" href="https://kurzschluss-blog.de/"><FontAwesomeIcon icon={faWordpress as IconProp} /> Blog</a></ListGroup.Item>
+                    <ListGroup.Item style={{border:0}}><a className="linkListLinks" href="http://kurzschlussjunkies.de/"><FontAwesomeIcon icon={faPodcast as IconProp} /> Podcast</a></ListGroup.Item>
+                    <ListGroup.Item style={{border:0}}><a className="linkListLinks" href="https://github.com/DasBasti"><FontAwesomeIcon icon={faGithub as IconProp} /> GitHub</a></ListGroup.Item>
+                    <ListGroup.Item style={{border:0}}><a className="linkListLinks" href="https://www.youtube.com/channel/UCPyRzwxnI9-pL5-50JkxY5Q"><FontAwesomeIcon icon={faYoutube as IconProp} /> Youtube</a></ListGroup.Item>
                 </ListGroup>
             </Col>
         </Row>
