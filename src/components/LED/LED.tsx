a2213d392881a95b933fb2da567b451452601256
Bastian Neumann <bneumann@kurzschluss-engineering.de>
2022-04-04 22:58:07 +0200

    Feature: Add search box for LEDs of user

--- src/components/LED/LED.tsx
+++ src/components/LED/LED.tsx
@@ -6,19 +6,24 @@ export type LEDProps = {
     owner: string;
     animation: boolean;
     last_seen: string;
+    highlight?: boolean;
 }
 
 export default function LED(props: LEDProps) {
 
-    let led_color = props.color ? "#" + props.color.toString(16).padStart(6, "0").slice(0, 6) : "#000000";
+    let led_color = "#000000";
+    if (props.owner && props.color) {
+        led_color = "#" + props.color.toString(16).padStart(6, "0").slice(0, 6)
+    }
     const now = new Date().getTime();
     const last_seen = new Date(props.last_seen).getTime();
     const time_diff = (now - last_seen) / 1000;
     const seen = time_diff < 60 * 60 * 8;
 
-    return <div 
-            className="led" 
-            style={{ backgroundColor: seen ? led_color : "#000000" }} 
-            title={`LED ${props.id}: ${props.owner}\nzuletzt gesehen: ${props.last_seen}`}>
-           </div>;
+    return <div
+        className={props.highlight ? "alerts-border led":"led"}
+        style={{ backgroundColor: seen ? led_color : "#000000", border: props.highlight ? "3px solid red" : "none" }}
+        title={`LED ${props.id}: ${props.owner}\nzuletzt gesehen: ${props.last_seen}`}>
+    </div>;
 }
+
--- src/screens/Panel.tsx
+++ src/screens/Panel.tsx
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useAsync } from 'react-async';
 import LED from '../components/LED';
+import { LEDProps } from '../components/LED/LED';
 
 const loadLEDs = async () =>
     await fetch("/panel").then(res => (res.ok ? res : Promise.reject(res))).then(res => res.json())
@@ -8,39 +9,70 @@ const loadLEDs = async () =>
 
 export default function Panel() {
     const { data, isLoading, run } = useAsync({ deferFn: loadLEDs })
+    const search_string = useRef<HTMLInputElement>(null);
+    const [panel_data, updatePanelData] = useState<Array<LEDProps>>();
 
-    useEffect(()=>{
-        run(loadLEDs)
-    },[run])
+    const search_for = (search: string | undefined) => {
+        if (!search)
+            return;
+        if (!data)
+            return;
+        var temp_data = [...data];
+        temp_data.map((led: any, idx: number) => {
+            if (led.owner?.toLowerCase() === search.toLowerCase()) {
+                temp_data[idx].highlight = true;
+                updatePanelData(temp_data);
+            } else {
+                temp_data[idx].highlight = false;
+            }
+        })
+        updatePanelData(temp_data);
+    }
+
+    useEffect(() => {
+        run(loadLEDs);
+    }, [run])
+
+    useEffect(() => {
+        if (data)
+            updatePanelData(data);
+    }, [updatePanelData, data])
 
     if (isLoading) return (<div>Loading...</div>)
-    if (data) {
+    if (panel_data) {
         let rows = [];
 
         for (let x = 0; x < 32; x++) {
             let row: Array<JSX.Element> = [];
             for (let y = 0; y < 32; y++) {
                 const offset = x * 32 + y;
-                if (data[offset]) {
-                    row.push(<td className='led-table-row'>
-                        <LED key={data[offset].id} {...data[offset]} />
+                if (panel_data[offset]) {
+                    row.push(<td className='led-table-row' key={panel_data[offset].id}>
+                        <LED {...panel_data[offset]} />
                     </td>);
                 } else {
-                    row.push(<td className='led-table-row'>
-                        <LED key="letze" animation={false} color={0} id={1024} owner={""} last_seen={""} />
+                    row.push(<td className='led-table-row' key="letze">
+                        <LED animation={false} color={0} id={1024} owner={""} last_seen={""} />
                     </td>);
                 }
             }
             rows.push(row);
         }
 
-        return <div style={{ display: 'flex', justifyContent: "center" }}><table className='led-table'>
-            {rows.map((row: Array<JSX.Element>, key: number) =>
-                <tr key={key}>
-                    {row.map((led) => led)}
-                </tr>)
-            }
-        </table></div>
+        return <div>
+            <input ref={search_string} onChange={() => { search_for(search_string.current?.value) }} />
+            <div style={{ display: 'flex', justifyContent: "center" }}>
+                <table className='led-table'>
+                    <tbody>
+                        {rows.map((row: Array<JSX.Element>, key: number) =>
+                            <tr key={key}>
+                                {row.map((led) => led)}
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
     }
 
     return (
