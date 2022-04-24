7c5a1daa16c5eebf0244b3ed291bb493e0c4002a
Bastian Neumann <neumann.bastian@gmail.com>
2022-04-24 22:45:22 +0200

    Panel: Add info text to LED panel

--- src/screens/Panel.tsx
+++ src/screens/Panel.tsx
@@ -4,6 +4,7 @@ import LED from '../components/LED';
 import { LEDProps } from '../components/LED/LED';
 import { Typeahead } from 'react-bootstrap-typeahead';
 import { debounce } from 'lodash';
+import { getUsername } from '../helper/login';
 
 const loadLEDs = async () =>
     await fetch("/pcb/panel").then(res => (res.ok ? res : Promise.reject(res))).then(res => res.json())
@@ -13,6 +14,8 @@ export default function Panel() {
     const { data, isLoading, run } = useAsync({ deferFn: loadLEDs })
     const [panel_data, updatePanelData] = useState<Array<LEDProps>>();
     const [usernames, setUsernames] = useState<Array<string>>([]);
+    const [username, setUsername] = useState<string>("");
+    const [color, setColor] = useState(0);
 
     const search_for = debounce((search: string | undefined) => {
         if (!search)
@@ -37,12 +40,17 @@ export default function Panel() {
     }, [run])
 
     useEffect(() => {
-        if (data){
-                        updatePanelData(data);
-                        setUsernames([...data.map((u:any)=>u.owner).filter((e:string)=>e?e:false)]         
-                        );
+        if (data) {
+            updatePanelData(data);
+            setUsernames([...data.map((u: any) => u.owner).filter((e: string) => e ? e : false)]
+            );
+            data.forEach((u: any) => {if(u.owner === username.toLowerCase()) setColor(u.color)});
         }
-    }, [updatePanelData, data])
+    }, [updatePanelData, data, username, setColor])
+
+    useEffect(()=>{
+        setUsername(getUsername());
+    },[])
 
     if (isLoading) return (<div>Loading...</div>)
     if (panel_data) {
@@ -66,13 +74,43 @@ export default function Panel() {
         }
 
         return <div>
-            <div style={{ position: 'absolute', margin: 5 }}>
-                <label htmlFor='search'>User: </label>
+            <div style={{ position: 'absolute', margin: 5, width: "250px" }}>
+                <label htmlFor='user_search_box'>Suche: </label>
                 <Typeahead
-                id='user_search_box'
-                    onChange={(selection) => { if(selection.length)search_for(selection[0] as string); }}
+                    id='user_search_box'
+                    onChange={(selection) => { if (selection.length) search_for(selection[0] as string); }}
                     options={usernames}
                 />
+                
+                <div id='info'>
+                    <p>Du hast deine LED als Chatteilnehmer bereits erhalten! Mit diesen Befehlen kannst du deine LED Farbe bestimmen:</p>
+                    <p>
+                        <ul><li>
+                            !led 1-255 1-255 1-255</li>
+                            <li>
+                                !led CSS3-Name (https://www.cssportal.com/css3-color-names)</li><li>
+                                !led #Hexwert
+                            </li></ul></p>
+                            <div id='led' style={{height:50, width:50, margin:5}}>
+                <LED animation={false} color={color} id={1025} owner={username} />
+                </div>
+                    <p>Ausschalten kannst du deine LED mit <strong>!led off</strong></p>
+
+                    <p>Mit <strong>!led info</strong> oder <strong>!led status</strong> kannst du abfragen wo deine LED ist.</p>
+
+                    <p>Es gibt ein paar Effekte für deine LED, die kannst du an dein !led Kommando anhängen:</p>
+                    <ul>
+                        <li>!led blink</li>
+                        <li>!led rainbow</li>
+                        <li>!led boom</li>
+                        <li>!led fastbow</li>
+                        <li>!led morse</li>
+                        <li>!led identify</li>
+                        <li>!led stop</li>
+                    </ul>
+                    <p>DISCOMODE kann mit <strong>!led disco</strong> aktiviert werden.</p>
+                    <p>Eine Runde Game of Life kannst du mit <strong>!led gol</strong> starten</p>
+                </div>
             </div>
             <div style={{ display: 'flex', justifyContent: "center" }}>
                 <table className='led-table'>
